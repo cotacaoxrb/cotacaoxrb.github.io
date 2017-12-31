7d6a5fe19a8c6c4f76265a5b82c498bdd8b8e5a8
Leandro Castro <leandro.lvc@hotmail.com>
2017-12-30 22:43:50 -0200

    readme.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cotação XRB para BRL
+
+## Gostou? Me pague um café: xrb_1zuawgy55qwzsag9zfgsdy73fhhgrfoggdrazr7x9a3iuq59c4yt7q5s3rpb
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,12 +2,26 @@ var app = angular.module('cotacaoApp',['720kb.socialshare']);
 
 app.controller('cotacaoCtrl', ['$scope', '$http', 'Socialshare', function($scope, $http, Socialshare) {
   var api_bitgrail = "https://bitgrail.com/api/v1/markets";
-  $scope.valor_cotacao = 100.35;
+  var api_mercatox = "https://mercatox.com/public/json24";
+  var api_foxbit = "https://api.blinktrade.com/api/v1/BRL/ticker";
+  $scope.valor_cotacao = 0;
+  $scope.carregando = true;
   $scope.getCotacao = function(){
-    $http.get(api_bitgrail)
-    .then(function(response) {
-      console.log(response);
+    $http.get(api_mercatox).then(function(response) {
+      //console.log(response);
+      var dados_xrb = response.data.pairs["XRB_BTC"];
+      var cotacao_xrb = dados_xrb.last;
+      //console.log(cotacao_xrb);
+      $http.get(api_foxbit).then(function(response_fox){
+        var cotacao_bitcoin = response_fox.data.last;
+        //console.log();
+        $scope.valor_cotacao = cotacao_bitcoin*cotacao_xrb;
+        $scope.carregando = false;
+      })
     });
   }
-  //$scope.getCotacao();
+  $scope.getCotacao();
+  setInterval(function(){
+    $scope.getCotacao();
+  }, 15000);
 }]);
